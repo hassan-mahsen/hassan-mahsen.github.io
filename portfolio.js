d78665ef6c26269adce8d420480fbab4ae25e525
hassan-mahsen <40634174+hassan-mahsen@users.noreply.github.com>
2018-06-27 16:40:40 +0300

    my first website
    my first portfolio

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+var clickMe=document.getElementById('clickMe');
+clickMe.addEventListener('click', function(){
+var name=prompt ('What is your name?')
+var yes=prompt('Hi'+ name + 'unataka chai?')
+alert('wazi' + name)
+})
